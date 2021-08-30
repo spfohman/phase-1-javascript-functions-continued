b7ddd93ad7c0986816573a7cd242b953a6249ba1
Sarah <spfohman@email.essex.edu>
2021-08-30 12:44:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(activity= 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(string = 'go to the office'){
+    return `This Monday, I will ${string}.`;
+}
+function wrapAdjective(parm='*'){
+	
+    return function(string='special'){
+        return `You are ${parm}${string}${parm}!`;
+    }
+}
